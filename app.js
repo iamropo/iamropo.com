1b5c1861568a45fee590db53b5240b2feaefeb7d
iamropo <iamropo@gmail.com>
2016-10-16 16:27:43 +0530

    made use of const for node modules included

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var http = require('http')
-var express = require('express')
-var app = express()
+const http = require('http');
+const express = require('express');
+var app = express();
 
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
 
-http.createServer(app).listen(1337, function() {console.log('Server Running...')})
+http.createServer(app).listen(1337, function() {console.log('Server Running...')});
