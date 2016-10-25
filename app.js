34017566bda2b2fddad43eef069343b1970d3518
iamropo <iamropo@gmail.com>
2016-10-25 17:52:15 +0530

    deleted all js files for fetching blog posts using file system

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const http = require('http');
 const express = require('express');
 var app = express();
-var blogPostsList = require('blog-posts-list')
 
 app.use('/', express.static('public'));
-app.get('/', function (req, res) {
-  res.sendFile()
-})
 
 http.createServer(app).listen(1337, function() {console.log('Server Running...')});
--- blog-post-list.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-// Pass dir(s) as argument to list all the contenst of that dir
-module.exports = function () {
-  return arguments.forEach(function (path) {
-    fs.readdir(path, function (err, posts) {
-      return posts;
-    })
-  })
-}
