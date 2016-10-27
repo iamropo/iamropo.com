fc032d638380feacfcd9e6eed4104544794e5c0a
iamropo <iamropo@gmail.com>
2016-10-27 12:10:16 +0530

    added dependency mongodb

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const express = require('express');
+const mongodb = require('mongodb');
+
 var app = express();
+var mongoClient = mongodb.MongoClient;
+var mongoURL = 'mongodb://localhost:27017/blog'; // 'blog' is the name of the database
 
 app.use('/', express.static('public'));
 
-http.createServer(app).listen(1337, function() {console.log('Server Running...')});
+http.createServer(app).listen(1337, function () {console.log('Server Running...')});
