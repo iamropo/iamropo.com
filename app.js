f70543486cf97cae45e26226c2d8442699e52c80
iamropo <iamropo@gmail.com>
2017-02-09 02:47:57 +0530

    mobile-layout

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+var data = require('./views/js/data.js')
+
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', data)
 })
 
 app.listen('3000', () => {
---
+++ views/js/data.js
@@ -0,0 +1,6 @@
+var data = {
+	about: `Hi! Myself Hage Ropo and I am a web developer. JavaScript is what I spend my time with the most.  
+	Apart from being a developer, I like soccer, art and good fonts.`
+}
+
+module.exports = data
