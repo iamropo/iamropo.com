22a3e2520f4b225fa8b79ffeb000b530c533658b
iamropo <iamropo@gmail.com>
2017-02-10 00:24:18 +0530

    added #hire-me

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 var data = require('./views/js/data.js')
 
 app.set('view engine', 'pug')
+app.use(express.static('views')) // for serving static files like images
 
 app.get('/', (req, res) => {
   res.render('index', data)
--- views/js/data.js
+++ views/js/data.js
@@ -1,6 +1,3 @@
-var data = {
-	about: `Hi! My name is Hage Ropo, I design and develop web applications. JavaScript is what I spend my time with the most.  
-	Also, I love soccer, concerts, art and good fonts.`
-}
+var data = null // just for now
 
 module.exports = data
